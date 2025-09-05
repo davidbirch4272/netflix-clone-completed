e9863d0279d1ab944e32e1a0394514c3b71b88e7
davidbirch4272 <davidbirch4272@gmail.com>
2025-09-05 15:47:25 -0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,7 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { auth } from "./firebase";
-import "./App.css";
-import Nav from "./components/Nav";
-import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen";
-import { useDispatch, useSelector } from "react-redux";
-import { login, logout, selectUser } from "./features/userSlice";
-import ProfileScreen from "./screens/ProfileScreen";
+import React from "react"
 
 function App() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const unsubsribe = auth.onAuthStateChanged((userAuth) => {
-      if (userAuth) {
-        // Logged In 
-      
-        dispatch(login({
-          uid: userAuth.uid,
-          email: userAuth.email,
-        })
-      );
-      }else{
-        // Logged Out
-        dispatch(logout());
-      
-      }
-    });
-
-    return unsubsribe;
-  }, [dispatch]);
-
-  return (
-    <div className="app">
-
-      <Router>
-        {!user ? (
-          <LoginScreen />          
-        ) : (        
-        <Routes>
-          <Route path="/profile" element={<ProfileScreen />} />
-          <Route exact path="/" element={<HomeScreen />} />
-        </Routes>        
-        )}
-        </Router>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
